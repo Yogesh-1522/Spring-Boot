c02028a08c94ff47f7de685f04f0cc7a05890621
DevilYogesh1522 <133919650+DevilYogesh1522@users.noreply.github.com>
2025-09-20 20:36:05 +0530

    using ResponseEntity

--- demo/src/main/java/com/example/demo/TodoController.java
+++ demo/src/main/java/com/example/demo/TodoController.java
@@ -1,17 +1,31 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@RestController
 public class TodoController {
-    private static List<Todo> todos;
+    private static List<Todo> todoList;
 
-     public TodoController(){
-         todos= new ArrayList<>();
-         todos.add(new Todo(1,false,10,"yogesh"));
-         todos.get(1);
-     }
-     @GetMapping("/todos")
+    public TodoController() {
+        todoList = new ArrayList<>();
+        todoList.add(new Todo(1, false, 10, "yogesh"));
+
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> gettodoList() {
+        return todoList;
+    }
+
+    @PostMapping("/todos")
+
+    public ResponseEntity<Todo> createtodo(@RequestBody Todo newTodo) {
+        todoList.add(newTodo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+    }
+
 }
